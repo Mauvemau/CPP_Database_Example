a60105337c7b06b905ad39442ae49e1071546f62
Mau <mauvepinktree@gmail.com>
2024-08-01 07:18:17 -0300

    Deleted comment

--- Game/src/main.cpp
+++ Game/src/main.cpp
@@ -7,57 +7,5 @@ using namespace std;
 int main() {
     Menu menu;
     menu.run();
-
-    /*
-    DatabaseManager databaseManager("localhost", "root", "1234", "my_game", 3306);
-    cout << "\n";
-
-    //system("pause");
-    myPause();
-    cout << "\n";
-    string username;
-    string password;
-
-    cout << "Insert the username you want to create.\n> ";
-    username = MiscUtils::usernameInput();
-
-    cout << "Insert the password you want to create.\n> ";
-    password = MiscUtils::passwordInput();
-
-    cout << "\n";
-
-    cout << "Your username is: " << username << ", your password is: " << password << "\n";
-
-    cout << "\n";
-
-    cout << "Inserting new user into the database...\n";
-
-    databaseManager.createUser(username, password);
-    */
-
-    /*
-    cout << "Insert the username you want to log in as.\n> ";
-    username = MiscUtils::usernameInput();
-
-    cout << "\n";
-    cout << "Retrieving new entry from the database...\n";
-
-    User user = databaseManager.getUser(username);
-
-    if (user.getID() != 0) {
-        cout << "User retrieved successfully!\n";
-        cout << "User ID: " << user.getID() << ".\nUser name: " << user.getName() << "\n";
-
-        do {
-            cout << "\nInsert user password.\n> ";
-            password = MiscUtils::passwordInput();
-        } while (!databaseManager.validatePassword(user.getID(), password));
-        cout << "Logged in!\n";
-    }
-    else {
-        cout << "Couldn't retrieve user";
-    }
-    */
-
     return 0;
 }
